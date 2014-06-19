11fc607b7662b661c40ef313e447190424951822
PadreZippo <britlucifer@gmail.com>
2014-06-19 12:50:34 -0400

    Creates user.meta.js

---
+++ user.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name       Sketchfab Model Debug
+// @namespace  https://github.com/PadreZippo/sketchfab-debug/
+// @version    0.1
+// @description  inserts button on model pages to load debug info
+// @include      https://sketchfab.com/models/*
+// @require http://code.jquery.com/jquery-latest.js
+// @require https://raw.githubusercontent.com/PadreZippo/sketchfab-debug/master/debug.js
+// ==/UserScript==
