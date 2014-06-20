6954981aba1fd99b5d03da3e3ad74682b4a73f99
PadreZippo <britlucifer@gmail.com>
2014-06-20 17:27:06 -0400

    Version 0.2.0
    Moves all JavaScript into main user.js
    Adds user email field
    Removes awkward model ID form/submit from both UI and script
    Adds comments (incomplete)
    Changes version to 0.2.0

--- user.js
+++ user.js
@@ -1,33 +1,191 @@
 // ==UserScript==
 // @name          Sketchfab Model Debug
 // @namespace     https://github.com/PadreZippo/sketchfab-debug/
-// @version       0.1.2
+// @version       0.2.0
 // @updateURL     https://raw.githubusercontent.com/PadreZippo/sketchfab-debug/master/user.js
 // @downloadURL   https://raw.githubusercontent.com/PadreZippo/sketchfab-debug/master/user.js
 // @description   inserts button on model pages to load debug info
 // @include       https://sketchfab.com/models/*
 // @require       http://code.jquery.com/jquery-latest.js
-// @require       https://raw.githubusercontent.com/PadreZippo/sketchfab-debug/master/debug.js
 // ==/UserScript==
 
-$(document).ready(function() {
-    $('div.additional div.actions').prepend('<a class="button btn-medium btn-secondary" id="debug">Debug</a>');
+// Add debug button
+$(document).ready(function () {
+  $('div.additional div.actions').prepend('<a class="button btn-medium btn-secondary" id="debug">Debug</a>');
 }());
 
-var modelId = window.location.pathname;
+// Get model ID
+var modelPath = window.location.pathname;
+var modelId = modelPath.replace('/models/', '');
 
+// Function to replace model viewer with debug info
 function openDebug() {
-    
-    var content = '<h2>Sketchfab model debug info    </h2>    <form id="form-model">      <div>        <label for="model">Model        </label>        <input type="text" id="model" value=window.location.pathname>        <input type="submit" id="submit" value="Go">      </div>    </form>    <h2>Mesh    </h2>    <div class="block">      <form>        <div>          <label>Vertices          </label>          <output id="vertices">          </output>        </div>        <div>          <label>Faces          </label>          <output id="faces">          </output>        </div>        <div>          <label>Geometries          </label>          <output id="geometries">          </output>        </div>      </form>    </div>    <h2>Thumbnail    </h2>    <div class="block">      <div id="thumbnail">      </div>    </div>    <h2>Settings (material editor)    </h2>    <div class="block">      <h3>Materials      </h3>      <ul id="settings-materials">      </ul>      <h3>Textures      </h3>      <form>        <div>          <label>Count          </label>          <output id="settings-textures-count">          </output>        </div>      </form>      <div id="settings-textures">      </div>    </div>    <h2>Default materials    </h2>    <div class="block">      <h3>Materials      </h3>      <ul id="model-materials">      </ul>      <h3>Textures      </h3>      <form>        <div>          <label>Count          </label>          <output id="model-textures-count">          </output>        </div>      </form>      <div id="model-textures">      </div>    </div>';
-    $('div.main').remove();
-    $('div.sections').prepend('<div class="main" id="debug">' + content + '</div>');
-    $('header').append('<a class="model-name" href="https://sketchfab.com' + modelId + '">Back</a>');
-    $('#debug').ready(function() {
-        $('#model').val(modelId);
-        start();
-        $('#submit').click();
-    }());
-}  
 
+  // Define debug markup and edit existing markup
+  var content = '<h2>Sketchfab model debug info    </h2>    <h2>Mesh    </h2>    <div class="block">      <form>        <div>          <label>Vertices          </label>          <output id="vertices">          </output>        </div>        <div>          <label>Faces          </label>          <output id="faces">          </output>        </div>        <div>          <label>Geometries          </label>          <output id="geometries">          </output>        </div>      </form>    </div>    <h2>Thumbnail    </h2>    <div class="block">      <div id="thumbnail">      </div>    </div>    <h2>Settings (material editor)    </h2>    <div class="block">      <h3>Materials      </h3>      <ul id="settings-materials">      </ul>      <h3>Textures      </h3>      <form>        <div>          <label>Count          </label>          <output id="settings-textures-count">          </output>        </div>      </form>      <div id="settings-textures">      </div>    </div>    <h2>Default materials    </h2>    <div class="block">      <h3>Materials      </h3>      <ul id="model-materials">      </ul>      <h3>Textures      </h3>      <form>        <div>          <label>Count          </label>          <output id="model-textures-count">          </output>        </div>      </form>      <div id="model-textures">      </div>    </div>';
+  $('div.main').remove();
+  $('div.sections').prepend('<div class="main" id="debug">' + content + '</div>');
+  $('header').append('<a class="model-name" href="https://sketchfab.com/models/' + modelId + '">Back</a>');
+  $('div.info-block.informations').before('<div class="info-block informations" style="margin-bottom: 20px;"><h5>About this user</h5><section><a id="email" href="mailto:"></a></section></div>');
+
+  // With new markup loaded, get debug info
+  $('#debug').ready(function () {
+    getModelInfo(modelId);
+  }());
+}
+
+// Open debug when debug button is clicked
 var button = document.getElementById("debug");
 button.addEventListener("click", openDebug, false);
+
+function displayTexture(texture) {
+  var imgs = $('<div>');
+  imgs.append($('<img/>').attr('src', texture.images[0].url));
+
+  if (texture.images.length > 0) {
+    //imgs.append($('<img/>').attr('src', texture.images[0].url));
+
+    texture.images.forEach(function (image) {
+      var a = $('<a/>').attr('href', image.url).attr('target', '_blank').text(image.width + 'x' + image.height);
+      imgs.append(a);
+      imgs.append('&nbsp;');
+    });
+  }
+
+  return imgs;
+}
+
+function humanSize(size) {
+  var suffixes = ['b', 'KiB', 'MiB', 'GiB'];
+
+  for (var i = 0; i < suffixes.length; i++, size /= 1024) {
+    if (size < 1024)
+      return size + suffixes[i];
+  }
+  return size + suffixes[suffixes.length - 1];
+}
+
+function getModelInfo(urlid) {
+  // Empty all model info fields
+  $('#thumbnail, #settings-materials, #settings-textures, #model-materials, #model-textures').empty();
+  var texturesSize = 0;
+
+  // Model info request URL and populate info fields from response
+  var now = Date.now(); // Get date
+  
+  // Get model basics
+  $.get('https://sketchfab.com/v2/models/' + urlid + '?' + now, function (data) {
+    $('#faces').val(data.faceCount);
+    $('#vertices').val(data.vertexCount);
+    Object.keys(data.options.materials).forEach(function (material_id) {
+      var material = data.options.materials[material_id];
+      $('#settings-materials').append($('<li>').text(material.name + ' (uid=' + material_id + ')'));
+    });
+      
+    // Get user email
+    $.get('https://sketchfab.com/v2/users/' + data.user.uid, function (user) {
+      var userMail = user.email;
+      var _href = $('#email').attr('href');
+      $('#email').attr('href', _href + userMail);
+      $('#email').html(userMail);
+    });
+    
+    // Get textures
+    $('#thumbnail').append(displayTexture(data.thumbnails));
+  });
+
+  $.get('https://sketchfab.com/v2/models/' + urlid + '/textures' + '?' + now, function (data) {
+    $('#settings-textures-count').text(data.results.length);
+    data.results.forEach(function (texture) {
+      console.log(texture);
+
+      var imgs = $('<div>');
+      if (texture.images.length > 0) {
+        texture.images.forEach(function (img) {
+          var url = img.url;
+          var image = new Image();
+
+          var a = $('<a/>').attr('href', url).attr('target', '_blank');
+          a.text('?x?');
+          imgs.append(
+            $('<img/>').attr({
+              src: url,
+              width: 64,
+              height: 64
+            }),
+            a
+          );
+
+          image.onload = function () {
+            var size = image.width * image.height * 4;
+            a.text(image.width + 'x' + image.height + ' (' + humanSize(size) + ')');
+          };
+          image.src = url;
+        });
+      } else {
+        imgs.append(texture.name + ' has no image');
+      }
+      $('#settings-textures').append(imgs);
+    });
+  });
+
+  $.get('https://media.sketchfab.com/urls/' + urlid + '/file.osgjs.gz' + '?' + now, function (json) {
+    var data = JSON.parse(json);
+    var geometryCount = 0;
+
+    var textures = {};
+
+    function traverse(children) {
+      for (var i in children) {
+        if (children.hasOwnProperty(i)) {
+          var node = children[i];
+
+          if (typeof (node) === 'object')
+            traverse(node);
+          if (i === 'osg.Geometry') {
+            geometryCount++;
+            $('#geometries').text(geometryCount);
+          } else if (i === 'osg.Texture' && node.File) {
+            var url = 'https://media.sketchfab.com/urls/' + urlid + '/' + node.File;
+            if (!textures[url]) {
+              textures[url] = true;
+            }
+          } else if (i === 'osg.Material') {
+            $('#model-materials').append($('<li>').text(node.Name));
+          }
+        }
+      }
+    }
+
+    traverse(data);
+
+    var texturesSize = 0;
+    for (var url in textures) {
+      (function () {
+        var image = new Image();
+
+        var a = $('<a/>').attr('href', url).attr('target', '_blank');
+        a.text('?x?');
+        $('#model-textures').append(
+          $('<div>').append(
+            $('<img/>').attr({
+              src: url,
+              width: 64,
+              height: 64
+            }),
+            a
+          )
+        );
+
+        image.onload = function () {
+          var size = image.width * image.height * 4;
+          a.text(image.width + 'x' + image.height + ' (' + humanSize(size) + ')');
+        };
+        image.src = url;
+      })();
+    }
+    $('#model-textures-count').text(Object.keys(textures).length);
+
+    $('#geometries').text(geometryCount);
+  });
+}
