6070c9a4cf23fe141b8b65973ee4b460356e03f8
PadreZippo <james@sketchfab.com>
2014-10-07 15:00:13 -0400

    Fixes model path

--- user.js
+++ user.js
@@ -1,13 +1,17 @@
 // ==UserScript==
 // @name          Sketchfab Model Debug
 // @namespace     https://github.com/PadreZippo/sketchfab-debug/
-// @version       0.3.3
+// @version       0.3.5
 // @updateURL     https://raw.githubusercontent.com/PadreZippo/sketchfab-debug/master/user.js
 // @downloadURL   https://raw.githubusercontent.com/PadreZippo/sketchfab-debug/master/user.js
 // @description   inserts button on model pages to load debug info
 // @include       https://sketchfab.com/models/*
 // @grant         none
 // ==/UserScript==
+
+// Get model ID
+var modelPath = window.location.pathname;
+var modelId = modelPath.replace('/models/', '');
 
 // Add debug button on page load
 $(document).ready(function () {
@@ -17,10 +21,6 @@ $(document).ready(function () {
   button.on('click', openDebug);
 }());
 
-// Get model ID
-var modelPath = window.location.pathname;
-var modelId = modelPath.replace('/models/', '');
-
 // Replace model viewer with debug info
 function openDebug() {
 
@@ -28,7 +28,7 @@ function openDebug() {
   var content = '<h2>  Model Debug</h2><h2>  Mesh</h2><div class="block">  <form>    <div>      <label>        Vertices:      </label>      <output id="vertices"></output>    </div>    <div>      <label>        Faces:      </label>      <output id="faces"></output>    </div>    <div>      <label>        Geometries:      </label>      <output id="geometries"></output>    </div><div>      <label>        Source:      </label>      <output id="source"></output>    </div><div>      <label>        Source Tool:      </label>      <output id="source-tool"></output>    </div><div>      <label>        Matrix Transform:      </label>      <output id="matrix-trans"></output>    </div><div>      <label>        Top Node Source:      </label>      <output id="top-node"></output>    </div>    <div>      <label>        UV Maps:      </label>      <output id="uvmaps"></output>    </div>  </form></div><h2>  Thumbnail</h2><div class="block">  <div id="thumbnail"></div></div><h2>  Material Settings</h2><div class="block">  <h3>    Materials (settings)  </h3>  <ul id="settings-materials"></ul>    <h3>    Textures  </h3>    <form>    <div>      <label>        Count      </label>      <output id="settings-textures-count"></output>    </div>  </form>    <div id="settings-textures"></div>  </div><h2>  Materials (default)</h2><div class="block">  <h3>    Materials  </h3>    <ul id="model-materials"></ul>  <h3>    Textures  </h3>    <form>    <div>      <label>        Count      </label>      <output id="model-textures-count"></output>    </div>  </form>    <div id="model-textures"></div>  </div>';
   $('.left').empty();
   $('.left').prepend('<div class="main" id="debug">' + content + '</div>');
-  $('.header').append('<a class="model-name" href="https://sketchfab.com/models/' + modelId + '">Back</a>');
+  $('.header').append('<a class="model-name" href="' + modelPath + '">Back</a>');
   $('div.info-block.informations').before('<div class="info-block informations" style="margin-bottom: 20px;"><h5>About this user</h5><section><a id="email" href="mailto:"></a></section></div>');
 
   // With new markup loaded, get debug info
