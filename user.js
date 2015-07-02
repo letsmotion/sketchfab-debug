c4abca0e552cb1da57a86af29c42f5bd92f58e68
PadreZippo <james@sketchfab.com>
2015-07-02 11:48:56 -0400

    Fixes OSGJS path after migration
    Adds SP button
    Bumps version

--- user.js
+++ user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name          Sketchfab Model Debug
 // @namespace     https://github.com/sketchfab/sketchfab-debug/
-// @version       0.3.9
+// @version       0.4.0
 // @updateURL     https://raw.githubusercontent.com/sketchfab/sketchfab-debug/master/user.js
 // @downloadURL   https://raw.githubusercontent.com/sketchfab/sketchfab-debug/master/user.js
-// @description   inserts button on model pages to load debug info
+// @description   Inserts buttons on model pages to load debug info and other tools
 // @include       https://sketchfab.com/models/*
 // @grant         none
 // ==/UserScript==
@@ -13,20 +13,22 @@
 var modelPath = window.location.pathname,
     modelId = modelPath.replace( '/models/', '' ),
     modelAdmin = 'https://sketchfab.com/admin/skfb_models/model/' + modelId,
+    modelAdminSearch = 'https://sketchfab.com/admin/skfb_models/model/?q=' + modelId,
     modelEdit = modelPath + '/edit?debug3d=1',
     modelInspect = 'http://sketchfab.github.io/experiments/model-inspector/index.html?urlid=' + modelId;
 
 // Add debug button on page load
 $( document ).ready( function() {
-  var debugButton = $( '<a class="button btn-medium btn-secondary">Debug</a>' ),
-      propButton = $( '<a class="button btn-medium btn-secondary">Props</a>' ),
-      editButton = $( '<a href="' + modelEdit + '" class="button btn-medium btn-secondary" target="_blank" >Edit</a>' ),
-      adminButton = $( '<a href="' + modelAdmin + '" class="button btn-medium btn-secondary" target="_blank" >Admin</a>' ),
-      inspectButton = $( '<a href="' + modelInspect + '" class="button btn-medium btn-secondary" target="_blank" >Inspect</a>' );
+  var debugButton = '<a id="debug" class="button btn-medium btn-secondary">Debug</a>',
+      propButton = '<a id="prop" class="button btn-medium btn-secondary">Properties</a>',
+      editButton = '<a href="' + modelEdit + '" class="button btn-medium btn-secondary" target="_blank">Edit</a>',
+      adminButton = '<a href="' + modelAdminSearch + '" class="button btn-medium btn-secondary" target="_blank">Staff Pick</a>',
+      spButton = '<a href="' + modelAdmin + '" class="button btn-medium btn-secondary" target="_blank">Admin</a>',
+      inspectButton = '<a href="' + modelInspect + '" class="button btn-medium btn-secondary" target="_blank">Inspect</a>';
   $( '[data-action="open-embed-popup"]' ).remove();
-  $( 'div.additional div.actions' ).prepend( inspectButton, debugButton, propButton, editButton, adminButton );
-  debugButton.on( 'click', openDebug );
-  propButton.on( 'click', openProps );
+  $( 'div.additional' ).after( '<div class="additional"><div class="actions" style="width: 100%;">' + inspectButton + debugButton + propButton + editButton + adminButton + spButton + '</div></div>' );
+  $( '#debug' ).on( 'click', openDebug );
+  $( '#prop' ).on( 'click', openProps );
 }());
 
 // Create properties dialog for editing models that don't belong to me
@@ -148,9 +150,104 @@ function getModelInfo( urlid ) {
   
   var texturesSize = 0,
       now = Date.now(); // Get date
+
+  // Get and parse polygon osgjs
+  function getOsgjs( osgjsUrl ) {
+    $.get( osgjsUrl + '?' + now, function( json ) {
+      var data = JSON.parse( json ),
+          geometryCount = 0,
+          uvCount = 0,
+          textures = {};
+
+      // Traverse polygon json to extract model data
+      function traverse( children ) {
+        for ( var i in children ) {
+          if ( children.hasOwnProperty( i ) ) {
+            var node = children[ i ];
+
+            if ( typeof ( node ) === 'object' ) {
+              traverse( node );
+            }
+
+            if ( i === 'osg.Geometry' ) {
+              geometryCount++;
+              $( '#geometries' ).text( geometryCount );
+            } else if ( i === 'osg.Texture' && node.File ) {
+              var url = 'https://media.sketchfab.com/urls/' + urlid + '/' + node.File;
+              if ( !textures[ url ] ) {
+                textures[ url ] = true;
+              }
+            } else if ( i === 'osg.Material' ) {
+              $( '#model-materials' ).append( $( '<li>' ).text( node.Name ) );
+            } else if ( i === 'UserDataContainer' ) {
+              var dataContainer = node.Values;
+              for ( var j = 0; j < dataContainer.length; j++ ) {
+                var dataValue = dataContainer[j];
+                if (dataValue.Name === 'source') {
+                  $( '#source' ).text( dataValue.Value );
+                } else if ( dataValue.Name === 'source_tool' ) {
+                  $( '#source-tool' ).text( dataValue.Value );
+                }
+              }
+            } else if ( i === 'osg.MatrixTransform' && node.UniqueID === 0 ) {
+              $( '#matrix-trans' ).text( sourceSplit( node.Name ) );
+            } else if ( i === 'osg.Node' && node.UniqueID === 0 ) {
+              $( '#top-node' ).text( sourceSplit( node.Name ) );
+            } else if ( i.indexOf( 'TexCoor' ) >= 0 ) {
+              uvCount++;
+              $( '#uvmaps' ).text( uvCount );
+            }
+          }
+        }
+      }
+
+      // Get the file name from root node path
+      function sourceSplit( nameStr ) {
+        var separator = "/",
+            nameArr = nameStr.split( separator ),
+            nameIndex = nameArr.length - 1,
+            nameSource = nameArr[ nameIndex ];
+        return nameSource;
+      }
+
+      traverse( data );
+
+      // Add default textures to markup
+      var texturesSize = 0;
+      for ( var url in textures ) {
+        ( function() {
+          var image = new Image(),
+              a = $( '<a/>' ).attr( 'href', url ).attr( 'target', '_blank' );
+          
+            a.text( '?x?' );
+          $( '#model-textures' ).append(
+            $( '<div>' ).append(
+              $( '<img/>' ).attr({
+                src: url,
+                width: 64,
+                height: 64
+              }),
+              a
+            )
+          );
+
+          image.onload = function() {
+            var size = image.width * image.height * 4;
+            a.text( image.width + 'x' + image.height + ' (' + humanSize( size ) + ')' );
+          };
+          image.src = url;
+        })();
+      }
+      $( '#model-textures-count' ).text( Object.keys( textures ).length );
+      $( '#geometries' ).text( geometryCount );
+    });
+  }
   
   // Get model basics
   $.get( 'https://api.sketchfab.com/i/models/' + urlid + '?' + now, function( data ) {
+    
+    var osgjsUrl = data.files.osgjsUrl;
+
     $( '#faces' ).val( data.faceCount );
     $( '#vertices' ).val( data.vertexCount );
     Object.keys( data.options.materials ).forEach( function( material_id ) {
@@ -160,6 +257,9 @@ function getModelInfo( urlid ) {
     
     // Get thumbnail
     $( '#thumbnail' ).append( displayTexture( data.thumbnails ) );
+
+    // Pass and get OSGJS
+    getOsgjs( osgjsUrl );
   });
 
   // Get textures
@@ -196,95 +296,5 @@ function getModelInfo( urlid ) {
       }
       $( '#settings-textures' ).append( imgs );
     });
-  });
-
-  // Get and parse polygon osgjs
-  $.get( 'https://media.sketchfab.com/urls/' + urlid + '/file.osgjs.gz' + '?' + now, function( json ) {
-    var data = JSON.parse( json ),
-        geometryCount = 0,
-        uvCount = 0,
-        textures = {};
-
-    // Traverse polygon json to extract model data
-    function traverse( children ) {
-      for ( var i in children ) {
-        if ( children.hasOwnProperty( i ) ) {
-          var node = children[ i ];
-
-          if ( typeof ( node ) === 'object' ) {
-            traverse( node );
-          }
-
-          if ( i === 'osg.Geometry' ) {
-            geometryCount++;
-            $( '#geometries' ).text( geometryCount );
-          } else if ( i === 'osg.Texture' && node.File ) {
-            var url = 'https://media.sketchfab.com/urls/' + urlid + '/' + node.File;
-            if ( !textures[ url ] ) {
-              textures[ url ] = true;
-            }
-          } else if ( i === 'osg.Material' ) {
-            $( '#model-materials' ).append( $( '<li>' ).text( node.Name ) );
-          } else if ( i === 'UserDataContainer' ) {
-            var dataContainer = node.Values;
-            for ( var j = 0; j < dataContainer.length; j++ ) {
-              var dataValue = dataContainer[j];
-              if (dataValue.Name === 'source') {
-                $( '#source' ).text( dataValue.Value );
-              } else if ( dataValue.Name === 'source_tool' ) {
-                $( '#source-tool' ).text( dataValue.Value );
-              }
-            }
-          } else if ( i === 'osg.MatrixTransform' && node.UniqueID === 0 ) {
-            $( '#matrix-trans' ).text( sourceSplit( node.Name ) );
-          } else if ( i === 'osg.Node' && node.UniqueID === 0 ) {
-            $( '#top-node' ).text( sourceSplit( node.Name ) );
-          } else if ( i.indexOf( 'TexCoor' ) >= 0 ) {
-            uvCount++;
-            $( '#uvmaps' ).text( uvCount );
-          }
-        }
-      }
-    }
-
-    // Get the file name from root node path
-    function sourceSplit( nameStr ) {
-      var separator = "/",
-          nameArr = nameStr.split( separator ),
-          nameIndex = nameArr.length - 1,
-          nameSource = nameArr[ nameIndex ];
-      return nameSource;
-    }
-
-    traverse( data );
-
-    // Add default textures to markup
-    var texturesSize = 0;
-    for ( var url in textures ) {
-      ( function() {
-        var image = new Image(),
-            a = $( '<a/>' ).attr( 'href', url ).attr( 'target', '_blank' );
-        
-          a.text( '?x?' );
-        $( '#model-textures' ).append(
-          $( '<div>' ).append(
-            $( '<img/>' ).attr({
-              src: url,
-              width: 64,
-              height: 64
-            }),
-            a
-          )
-        );
-
-        image.onload = function() {
-          var size = image.width * image.height * 4;
-          a.text( image.width + 'x' + image.height + ' (' + humanSize( size ) + ')' );
-        };
-        image.src = url;
-      })();
-    }
-    $( '#model-textures-count' ).text( Object.keys( textures ).length );
-    $( '#geometries' ).text( geometryCount );
   });
 }
